a2c2c61fd5b34023627c03f942749f283b157fdb
Life-seeded <46518006+Life-seeded@users.noreply.github.com>
2020-08-21 13:54:11 +0900

    Update run.py

--- run.py
+++ run.py
@@ -61,16 +61,29 @@ async def _TesterRole(ctx, member: discord.Member=None):
     global tester
     await ctx.channel.send(str(tester) + "명의 테스터들이 존재합니다.")
 
-@client.event
-async def on_message(message):
-    await client.process_commands(message)
+@client.command(name="Hello", pass_context=True)
+async def _TesterRole(ctx, member: discord.Member=None):
+    await ctx.channel.send("Hello, World!")
 
-    if message.author == client.user:
-        return
+@client.command(name="인삿말", pass_context=True)
+async def _TesterRole(ctx, member: discord.Member=None):
+    embed=discord.Embed(title= f"안녕하세요. 부서진 마천루에 오신것을 환영합니다.", description=f"", color=0xf3bb76)
+    embed.add_field(name=f"해당 질문에 답변해 주시길 바랍니다.",value=f"1.들어온 경로를 말씀해주세요.\n2.들어온 이유를 말씀해주세요.",inline=False)
+    embed.add_field(name=f"모드 테스트를 위한 안내사항.",value=f"모드 테스트 목적으로 오셨다면 +테스터를 쳐주시면 자동적으로 테스터 권한을 받을수 있습니다.",inline=False)
+    embed.add_field(name=f"#테스터-안내사항",value=f"모드 설치 방법",inline=True)
+    embed.add_field(name=f"#테스트팀",value=f"다른 테스터들과 대화 또는 게임상황 공유",inline=True)
+    embed.add_field(name=f"#버그-건의, #좆같은점, #그래픽문제들",value=f"문제점 건의나 버그 제보",inline=False)
+    await ctx.send(embed=embed)
 
-    if message.content.startswith('+Hello'):
-        await message.channel.send('Hello, World!')
-
+@client.command(name="help", pass_context=True)
+async def _TesterRole(ctx, member: discord.Member=None):
+    embed=discord.Embed(title= f"Imperator Rome Bot", description=f"임페라토르 롬 봇의 명령어를 출력합니다.", color=0xf3bb76)
+    embed.set_author(name="Imperator Rome", url="", icon_url="https://upload.wikimedia.org/wikipedia/en/0/04/Imperator_Rome_logo.png")
+    embed.add_field(name=f"+테스터",value=f"테스터 역할을 부여합니다.",inline=True)
+    embed.add_field(name=f"+테스터숫자",value=f"테스터들의 숫자를 출력합니다.",inline=True)
+    embed.add_field(name=f"+인삿말",value=f"안내사항을 출력합니다.",inline=True)
+    embed.add_field(name=f"+Hello",value=f"Hello, World!를 출력합니다.",inline=True)
+    await ctx.send(embed=embed)
 
 if __name__ == "__main__":
     client.run(TOKEN)
